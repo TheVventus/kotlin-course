aa8c5857dde510c6cb50cb38d177244f3fa07fb2
Ildar Khayrullov <ildar.hayrullov@ligastavok.ru>
2025-09-14 19:54:32 +0400

    feature: done lesson 4

---
+++ src/main/kotlin/stimmax/lessons/lesson04/homework/Datatypes.kt
@@ -0,0 +1,53 @@
+package stimmax.lessons.lesson04.homework
+
+val v1: Byte = 42
+val v2: Long = 98765432123456789L
+val v3: Float = 23.45f
+val v4: Double = 0.123456789
+val v5: String = "Kotlin & Java"
+// Для типа Boolean значение переменной должно начинаться с маленькой буквы
+//val v6 = FALSE
+val v7: Char = 'c'
+val v8: Int = 500
+val v9: Long = 4294967296L
+val v10: Float = 18.0f
+val v11: Double = -0.001
+val v12: String = "OpenAI"
+// Для типа Boolean значение без кавычек, если хотим именно такое значение строки, то будет тип String
+val v13: String = "true"
+val v14: List<Byte> = listOf(3, 14)
+val v15: Char = '9'
+val v16: Int = 2048
+val v17: Long = 10000000000L
+val v18: Set<String> = setOf("OpenAI", "Quantum Computing")
+val v19: Float = 5.75f
+// Ошибка с обратными кавычками
+//val v20 = `1.414`
+val v21: String = "Artificial Intelligence"
+val v22: Array<Any> = arrayOf('x', "A")
+val v23: String = "Android Studio"
+val v24: Char = '@'
+val v25: Int = 1024
+val v26: Long = 1234567890123L
+val v27: Float = 10.01f
+val v28: Double = -273.15
+val v29: String = "SpaceX"
+// Для типа Boolean значение переменной должно начинаться с маленькой буквы
+//val v30 = FALSE
+val v31: Double = 0.007
+//Неправильные кавычки
+//val v32 = “🤯”
+val v33: Map<String, Int> = mapOf("true" to 2, "false" to 34)
+// Ошибка с кавычками
+//val v34 = ‘65535’
+val v35: Long = 72057594037927935L
+val v36: Float = 2.71828f
+val v37 : Double= 101.0101
+val v38: String = "Quantum Computing"
+val v39: Map <Int, String> = mapOf(2 to "true", 34 to "false")
+val v40: Char = 'x'
+val v41: Int = 314
+val v42: Long = 123456789123456789L
+val v43: Float = 6.626f
+// Для типа Boolean значение переменной должно начинаться с маленькой буквы
+//val v44 = TRUE
